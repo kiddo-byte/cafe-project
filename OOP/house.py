d6b3359d2809b8c90b2aafbbfb1649c14cc2cd5d
eMobilis <seanorina@gmail.com>
2023-02-13 10:04:27 +0300

    functions

---
+++ OOP/house.py
@@ -0,0 +1,8 @@
+class House1():
+    doors = 4
+    windows = 16
+    kitchen = 1
+    dinning = 2
+
+house1obj = House1()
+print(house1obj.doors, house1obj.dinning, house1obj.kitchen, house1obj.windows)
