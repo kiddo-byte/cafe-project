5d213248e421b6fb5abf265c69296a417d470afc
eMobilis <seanorina@gmail.com>
2023-02-13 09:17:00 +0300

    functions

---
+++ functions/functions.py
@@ -0,0 +1,9 @@
+def sayhello():
+    print("Do you want to shut down the machine")
+
+sayhello()
+
+def restartmachine():
+    print("Restart machine")
+
+restartmachine()
---
+++ functions/functions_exercise2.py
@@ -0,0 +1,6 @@
+def calculate_product():
+    first_number = int(input("Enter the first number"))
+    second_number = int(input("Enter the second number"))
+    result = first_number * second_number
+    print(f"The product is {result}")
+calculate_product()
