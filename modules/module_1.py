0dbe96da3f2914fb3ba832c2f4b5097b0aa68940
eMobilis <seanorina@gmail.com>
2023-02-14 08:56:57 +0300

    python

---
+++ modules/module_1.py
@@ -0,0 +1,12 @@
+def add_two_numbers(num1, num2):
+    return num1 + num2
+
+def multiply_two_numbers(num1, num2):
+    return num1 * num2
+
+def divide_two_numbers(num1, num2):
+    return num1 / num2
+
+def subtract_two_numbers(num1, num2):
+    return num1 - num2
+
---
+++ modules/module_2.py
@@ -0,0 +1,6 @@
+from module_1 import *
+
+print(add_two_numbers(34, 56))
+print(multiply_two_numbers(12, 10))
+print(divide_two_numbers(44, 2))
+print(subtract_two_numbers(43, 13))
